0d2092ac189fb9c6c48889357d04fa1c253afd15
kendallcruz <kendall.cruzsosa@ucr.ac.cr.com>
2022-07-24 00:11:29 -0600

    Realizo y comento el código para el servidor. El circuito físico consta de un sensor de llamas y de una salida que activa un led y un buzzer

--- ModbusServer/ModbusServer.ino
+++ ModbusServer/ModbusServer.ino
@@ -1,54 +1,65 @@
-/*
-  Modbus-Arduino Example - Master Modbus IP Client (ESP8266/ESP32)
-  Read Holding Register from Server device
-  (c)2018 Alexander Emelianov (a.m.emelianov@gmail.com)
-  https://github.com/emelianov/modbus-esp8266
-*/
-
 #ifdef ESP8266
  #include <ESP8266WiFi.h>
-#else
+#else //ESP32
  #include <WiFi.h>
 #endif
 #include "ModbusIP_ESP8266.h"
 
-const int REG = 100;               // Modbus dirección o Offset del holding register
-IPAddress remote(192, 168, 0, 10);  // Esta es la dirección de un dispositivo esclavo, use ModBus Slave (Asigne aquí la dirección IP de su PC donde está instalado este software)
-const int LOOP_COUNT = 10;
+//Modbus Registers Offsets
+const int SENSOR_IREG = 101; //Aquí tenemos la ubicación de la información del sensor de llama
+const int LED_COIL = 100; //Aquí tenemos la ubicación de la información para el output
+//Used Pins
+const int ledPin = 23; //GPIO23
 
-ModbusIP mb;  //ModbusIP object
+//Llamamos al objeto ModbusIP
+ModbusIP mb;
+
+
+long ts;
+
 
 void setup() {
-  Serial.begin(115200);
- 
-  WiFi.begin("linksys"); //Este es el nombre del SSID, se puede añadir el password en el segundo argumento
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
- 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    //velocidad máxima de baudios
+    Serial.begin(115200);
+    //Información de la red a la que se desea conectar
+    WiFi.begin("ARRIS-4A42", "BNX6C1301775");
+    //Método para que imprima la info en caso de conectarse
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-  mb.client(); //Inicio en modo cliente de ESP32
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP()); //imprime la IP local del servidor
+    
+    
+
+    mb.server();    //Encendemos el servidor Modbus IP
+    // Agregamos el registro SENSOR_IREG
+    // Se debe usaraddIreg() para entradas análogas
+    mb.addIreg(SENSOR_IREG);
+
+    //este método imprime el tiempo desde que se inició el programa
+    ts = millis();
+
+    //establecemos el pin que queremos como salida
+    pinMode(ledPin, OUTPUT);
+    mb.addCoil(LED_COIL);
 }
 
-uint16_t res = 0;
-uint8_t show = LOOP_COUNT;
-
 void loop() {
-  if (mb.isConnected(remote)) {   // Check if connection to Modbus Slave is established
-    mb.readHreg(remote, REG, &res);  //Leer un holding register en la dirección asignada en la IP remota.
-  } else {
-    mb.connect(remote);           //REconectar si hay problema
-  }
-  mb.task();                      // Ejecutar funciones de cliente modbus
-  delay(100);                     // Pulling interval
-  if (!show--) {                   // Se muestran los resultados de esta manera cada segundo, también se puede usar milis
-    Serial.println(res);
-    show = LOOP_COUNT;
-  }
+   //Empezamos el codigo para escribir en el output de manera binaria
+   mb.task();
+   digitalWrite(ledPin, mb.Coil(LED_COIL));
+   delay(10);
+   //La lectura se dará cada 2 segundos
+   if (millis() > ts + 2000) {
+       ts = millis();
+       //Setting raw value (0-1024)
+       mb.Ireg(SENSOR_IREG, analogRead(A0));
+   }
+   delay(10);
+
 }
